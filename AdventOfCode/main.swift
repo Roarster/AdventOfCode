b9fef13ed7ba9a7f6a3b579e739e240df7c1a704
Ian Strachan <ian@overlap.se>
2015-12-05 19:40:08 +0000

    Initial Commit

---
+++ AdventOfCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode
+//
+//  Created by Ian Strachan on 05/12/2015.
+//  Copyright © 2015 Ian Strachan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
